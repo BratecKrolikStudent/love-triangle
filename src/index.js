b41753bd6d78bd6be5afd1c0bbe6e58738840157
Dmitriy Kunets <t38@tut.by>
2018-09-10 23:27:43 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let triangles = new Set();
+
+  for (let spichonee = 0; spichonee < preferences.length; spichonee++) {
+    let spichonee1 = preferences[spichonee];
+    let spichonee2 = preferences[spichonee1 - 1];
+    let spichonee3 = preferences[spichonee2 - 1];
+
+    if (spichonee + 1 === spichonee3 && spichonee3 !== spichonee2) {
+      triangles.add([spichonee1, spichonee2, spichonee3].sort().join(''));
+    };
+  }
+
+  return triangles.size;
 };
